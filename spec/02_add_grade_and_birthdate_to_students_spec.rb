db10c73b45bf1dfc5c48113cde01ab331acff01c
Jose Soto <jsoto7087@gmail.com>
2023-02-04 08:04:24 -0500

    commit tree 1077c7db0121b03677f02087f76b2dde5fd2633c

--- spec/02_add_grade_and_birthdate_to_students_spec.rb
+++ spec/02_add_grade_and_birthdate_to_students_spec.rb
@@ -14,12 +14,12 @@ describe AddGradeAndBirthdateToStudents do
   end
 
   it 'has a grade' do
-    student = Student.create(name: "Steven", grade: 12, birthdate: "April 5th")
+    student = Student.create(name: "Steven", grade: 12, birthday: "April 5th")
     expect(Student.where(grade: 12).first).to eq(student)
   end
 
-  it 'has a string birthdate' do
-    student = Student.create(name: "Steven", grade: 12, birthdate: "April 5th")
-    expect(Student.where(birthdate: "April 5th").first).to eq(student)
+  it 'has a string birthday' do
+    student = Student.create(name: "Steven", grade: 12, birthday: "April 5th")
+    expect(Student.where(birthday: "April 5th").first).to eq(student)
   end
 end
